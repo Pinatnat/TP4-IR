0510779df2240a37851a1efd2d474d8326b7c6b3
Pinatnat <nad.nadya124@gmail.com>
2022-12-09 11:33:14 +0700

    implement search system

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import useSearchImpl from './pages/Search/useSearchImpl';
+import { render } from '@testing-library/react';
 
 function App() {
+  const { docs, onChange, onSubmit, renderDocs } = useSearchImpl()
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"></link>
+      </head>
+      <body>
+      <div className="App m-5">
+        <h1 className='center mb-5'>Medical Search</h1>
+        <div className='container'>
+          <form onSubmit={onSubmit}>
+          <div className="input-group mb-3">
+            <input type="text" className="form-control" placeholder="Search me" name='query' onChange={onChange}aria-describedby="basic-addon2"></input>
+            <div className="input-group-append">
+              <button className="btn btn-primary" type="submit">Search</button>
+            </div>
+          </div>
+          </form>
+        </div>
+      </div>
+      {renderDocs()}
+      </body>
+    </>
   );
 }
 
---
+++ src/pages/Search/useSearchImpl.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import { render } from "react-dom";
+
+const useSearchImpl = ()=> {
+    const [docs, setDocs] = useState<Doc[]>();
+    const [values, setValues] = useState<string>("");
+    
+    const search = async (input:string) => {
+        setDocs(await getConvertedData(input))
+    }
+    
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValues(event.target.value);
+    };
+
+    const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        await search(values); // triggering the callback
+    };
+
+    const renderDocs = () => {
+        return(<div className='container'>
+        {docs?.map((d)=> 
+        <>
+          <div className='container p-2 border-bottom border-secondary m-2'>
+            <h5>{d.doc}</h5>
+            <p>{d.content.substring(0,500)} <a href='#'>... See More</a></p>
+          </div>
+        </>
+        )}
+      </div>)
+      }
+    
+      useEffect(()=>{
+        renderDocs()
+      },[docs])
+    
+    
+    return { docs, onChange, onSubmit, renderDocs }
+}
+
+
+interface Doc {
+    doc: string
+    content: string
+}
+
+async function getConvertedData(input:string): Promise<Doc[]>{
+	return fetch('http://34.128.65.212:8000/search?query='+input, {
+		method: 'GET',
+	}).then(response => {
+        console.log(response)
+        return response.json()
+      }).then(data => { /* <-- data inferred as { data: T }*/
+      return data.data
+    })
+    }
+
+export default useSearchImpl;
